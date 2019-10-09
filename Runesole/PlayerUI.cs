8a2201a99c98ba85ac2325e36cf44b97ba7a25f2
Yuya Yoshino <yuyayoshino7@gmail.com>
2019-10-09 14:47:05 -0700

    Fixed up minor player and enemy data

--- Runesole/PlayerUI.cs
+++ Runesole/PlayerUI.cs
@@ -22,14 +22,14 @@ namespace Runesole
 
             UI.String(
                 Coord.TopLeft + Coord.Down + Coord.Down + Coord.Right *2, 
-                "Health: " + player.health + " / " + player.maxHealth, /// TODO make green, background black
+                "Health: " + Mathf.FloorToInt(player.health) + " / " + Mathf.FloorToInt(player.maxHealth), /// TODO make green, background black
                 Color.Forground.Green,
                 Color.Background.Black
             );
 
             UI.String(
                 Coord.TopLeft + Coord.Down + Coord.Down + Coord.Down + Coord.Right * 2, 
-                "Exp: " + player.experience + " / " + player.maxExperience, ///TODO make yellow, background black
+                "Exp: " + Mathf.FloorToInt(player.experience) + " / " + Mathf.CeilToInt(player.maxExperience), ///TODO make yellow, background black
                 Color.Forground.Yellow,
                 Color.Background.Black
             );
