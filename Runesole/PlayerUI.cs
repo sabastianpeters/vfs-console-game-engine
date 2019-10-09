7be17d57dd1d4a2db548a61abe28ada58f8443bf
Yuya Yoshino <yuyayoshino7@gmail.com>
2019-10-09 14:12:48 -0700

    Changed player and enemies

--- Runesole/PlayerUI.cs
+++ Runesole/PlayerUI.cs
@@ -14,21 +14,28 @@ namespace Runesole
         {
             //player.health 
             UI.String(
-                Coord.TopLeft + Coord.Right *2, 
+                Coord.TopLeft + Coord.Down + Coord.Right * 2,
+                "Level: " + player.level, ///TODO make yellow, background black
+                Color.Forground.Magenta,
+                Color.Background.Black
+            );
+
+            UI.String(
+                Coord.TopLeft + Coord.Down + Coord.Down + Coord.Right *2, 
                 "Health: " + player.health + " / " + player.maxHealth, /// TODO make green, background black
                 Color.Forground.Green,
                 Color.Background.Black
             );
 
             UI.String(
-                Coord.TopLeft + Coord.Down + Coord.Right * 2, 
+                Coord.TopLeft + Coord.Down + Coord.Down + Coord.Down + Coord.Right * 2, 
                 "Exp: " + player.experience + " / " + player.maxExperience, ///TODO make yellow, background black
                 Color.Forground.Yellow,
                 Color.Background.Black
             );
 
             UI.String(
-                Coord.TopLeft + Coord.Down + Coord.Down + Coord.Right * 2,
+                Coord.TopLeft + Coord.Down + Coord.Down + Coord.Down + Coord.Down + Coord.Right * 2,
                 "Str: " + player.attackDmg, ///TODO make yellow, background black
                 Color.Forground.Red,
                 Color.Background.Black
