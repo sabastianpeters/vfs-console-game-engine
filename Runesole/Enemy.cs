93dbd09909d3802655480f432bddbd3f98ee6527
Sabastian <sabastianp@hotmail.ca>
2019-10-09 18:43:56 -0700

    Added attack
    Enemies do damage
    Player does damage

--- Runesole/Enemy.cs
+++ Runesole/Enemy.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Runesole.Engine;
 
 namespace Runesole
 {
     class Enemy : LivingEntitiy
     {
-        
-    }
+		public override void Attack(LivingEntitiy target)
+		{
+			// if the target is in range of our attack range, attack them
+			if (target.IsInRange(position, attackRange))
+				target.TakeDamage(attackDmg * Time.deltaTime);
+		}
+	}
 }
