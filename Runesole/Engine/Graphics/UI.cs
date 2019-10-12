5be27c60a0c084e56a0141949c51785e81da5f3f
Sabastian <sabastianp@hotmail.ca>
2019-10-11 22:31:56 -0700

    prompt test code

--- Runesole/Engine/Graphics/UI.cs
+++ Runesole/Engine/Graphics/UI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,8 @@ namespace Runesole.Engine.Graphics
 	{
 		
 		private static Action drawQueue;
+
+		public static int promptWidth = 40;
 		
 		// ## CONST UTILITY VARIABLES ## // Make it easier to change values
 		private const Color.Foreground DEFAULT_FG = Color.Foreground.White;
@@ -46,7 +49,7 @@ namespace Runesole.Engine.Graphics
 		// draws a string starting from center of string
 		public static void StringCenter(Coord pos, string s, Color.Foreground fg = DEFAULT_FG, Color.Background bg = DEFAULT_BG)
 		{
-			pos.x -= s.Length / 2;              /// draws from center of string
+			pos.x -= (s.Length / 2);            /// draws from center of string
 			drawQueue += () => {                /// adds draw call to queue
 				_drawString(pos, s, fg, bg);
 			};
@@ -61,11 +64,11 @@ namespace Runesole.Engine.Graphics
 			};
 		}
 
-		//// asks user for an input
-		//public static int Prompt (string question, params string[] options)
-		//{
-			
-		//}
+		// asks user for an input, callback is returned with result
+		public static void Prompt (string question, Action<int> callback, params string[] options)
+		{
+			drawQueue += () => CoroutineManager.Call(_prompt(question, callback, options));
+		}
 
 
 		// ## PRIVATE UTITLITY METHODS ## // Things to make drawing easier
@@ -87,6 +90,99 @@ namespace Runesole.Engine.Graphics
 				ConsoleRenderer.SetColor(x, pos.y, bg);
 				x++;
 			}
+		}
+
+		// Prompt Coroutine
+		private static IEnumerator _prompt (string question, Action<int> callback, params string[] options)
+		{
+			GameManager.PauseGame();
+
+			
+
+			Sprite promptSprite = _generatePromptSprite(options.Length);
+
+			while (!Input.GetKeyDown(System.Windows.Input.Key.L))
+			{
+				promptSprite.Draw(Coord.TopLeft);
+				Debug.Log("YO");
+				yield return null;
+			}
+
+			GameManager.UnpauseGame();
+		}
+
+		private static void _drawPrompt (string[] promptDrawList)
+		{
+			int halfHeight = promptDrawList.Length / 2; /// prompt list of rows, half length is half height
+			for(int i = 0, y = -halfHeight; y < halfHeight; y++, i++)
+			{
+				StringCenter(Coord.Center + Coord.Down * y, promptDrawList[i], Color.Foreground.White, Color.Background.Black);
+			}
+		}
+
+
+		//private static string[] _generatePromptStrings (string str)
+		//{
+		//	string[] words = str.Split(' ');               // a list of words to write to screen
+
+		//	StringBuilder currentLine = new StringBuilder();    /// a string builder holding current line (starts with left border)
+		//	currentLine.Append('║');
+		//	int spaceRemaining = textWidth - currentLine.Length; /// how much space is left in prompt line
+		//	for (int i = 0; i < words.Length; i++)
+		//	{
+
+
+		//		// adds the words if they can fit
+		//		if (words[i].Length < spaceRemaining)
+		//		{
+		//			currentLine.Append(words[i]);
+		//		}
+		//		else // adds spaces if it can't fit
+		//		{
+		//			currentLine.Insert(0, new String(' ', spaceRemaining / 2)); /// adds remaining spaces
+		//			currentLine.Append(new String(' ', spaceRemaining / 2 + 1));
+		//			currentLine.Append('║'); /// add right border
+		//			promptDrawList.Add(currentLine.ToString()); /// adds string builder data and resets it
+		//			currentLine.Clear();
+		//			currentLine.Append('║'); /// add left border
+		//		}
+
+		//		spaceRemaining = textWidth - currentLine.Length + 1;
+		//	}
+		//}
+
+		private static Sprite _generatePromptSprite (int height)
+		{
+			int textWidth = promptWidth - 2;	/// how much space text can take up
+			int promptHeight = height + 2;		/// overall prompt height
+
+			Sprite promptSprite = new Sprite(promptWidth, promptHeight, new Coord(promptWidth/2, promptHeight/2), new Spit(' ', Color.Background.Black));
+
+			Spit xBarSpit = new Spit('═', Color.Foreground.White, Color.Background.Black);
+			Spit yBarSpit = new Spit('║', Color.Foreground.White, Color.Background.Black);
+
+			// draws top and bottom bars
+			for (int x = 1; x < promptWidth; x++)
+			{
+				promptSprite.SetSpit(x, 0, xBarSpit);
+				promptSprite.SetSpit(x, promptHeight-1, xBarSpit);
+			}
+
+			// draws left and right bars
+			for (int y = 1; y < promptHeight; y++)
+			{
+				promptSprite.SetSpit(0, y, xBarSpit);
+				promptSprite.SetSpit(promptWidth-1, y, xBarSpit);
+			}
+
+
+			// sets corners
+			promptSprite.SetSpit(0, 0, new Spit('╔', Color.Foreground.White, Color.Background.Black));
+			promptSprite.SetSpit(promptWidth-1, 0, new Spit('╗', Color.Foreground.White, Color.Background.Black));
+			promptSprite.SetSpit(0, promptHeight-1, new Spit('╚', Color.Foreground.White, Color.Background.Black));
+			promptSprite.SetSpit(promptWidth-1, promptHeight-1, new Spit('╝', Color.Foreground.White, Color.Background.Black));
+
+			return promptSprite;
 		}
 	}
 }
